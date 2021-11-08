94032467a13cea6584c80e591604890f1df16789
Dodo9128 <wodbs9128@gmail.com>
2021-11-08 19:03:08 +0900

    fix

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,11 +1,11 @@
 module Types
-    class AuthorType < Types::BaseObject
-        field :books, [Types::BookType], null: true
+  class AuthorType < Types::BaseObject
+    field :books, [Types::BookType], null: true
 
-        field :id, ID, null: false
+    field :id, ID, null: false
 
-        field :date_of_birth, String, null: false
-        field :first_name, String, null: false
-        field :last_name, String, null: false
-    end
+    field :date_of_birth, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+  end
 end
